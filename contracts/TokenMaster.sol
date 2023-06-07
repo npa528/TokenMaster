98bfb5e185821850501f816fe1bf4bd93d509e88
nikos papadakis <npapadakis@gmail.com>
2023-06-07 09:52:12 +0300

    contracts finished

--- contracts/TokenMaster.sol
+++ contracts/TokenMaster.sol
@@ -19,6 +19,7 @@ contract TokenMaster is ERC721 {
     }
 
     mapping(uint256 => Occasion) occasions;
+    mapping(uint256 => mapping(address => bool)) public hasBought;
     mapping(uint256 => mapping(uint256 => address)) public seatTaken;
     mapping(uint256 => uint256[]) seatsTaken;
 
@@ -56,7 +57,19 @@ contract TokenMaster is ERC721 {
     }
 
     function mint(uint256 _id, uint256 _seat) public payable {
+        // Require that _id is not 0 or less than total occasions
+        require(_id != 0);
+        require(_id <= totalOccasions);
+
+        // Require that ETH sent is greater than cost
+        require(msg.value >= occasions[_id].cost);
+
+        // Require that the seat is not taken and tha seat exits
+        require(seatTaken[_id][_seat] == address(0));
+        require(_seat <= occasions[_id].maxTickets);
+
         occasions[_id].tickets -= 1; // Update ticket count
+        hasBought[_id][msg.sender] = true; // Update buying status
         seatTaken[_id][_seat] = msg.sender; // Assign seat
         seatsTaken[_id].push(_seat); // Update seats currently taken
         totalSupply++;
@@ -65,5 +78,14 @@ contract TokenMaster is ERC721 {
 
     function getOccasion(uint256 _id) public view returns (Occasion memory) {
         return occasions[_id];
+    }
+
+    function getSeatsTaken(uint256 _id) public view returns (uint256[] memory) {
+        return seatsTaken[_id];
+    }
+
+    function withdraw() public onlyOwner {
+        (bool success, ) = owner.call{value: address(this).balance}("");
+        require(success);
     }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,22 +6,83 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+const tokens = (n) => {
+  return ethers.utils.parseUnits(n.toString(), "ether");
+};
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  // Setup accounts & variables
+  const [deployer] = await ethers.getSigners();
+  const NAME = "TokenMaster";
+  const SYMBOL = "TM";
 
-  const lockedAmount = hre.ethers.utils.parseEther("0.001");
+  // Deploy contract
+  const TokenMaster = await ethers.getContractFactory("TokenMaster");
+  const tokenMaster = await TokenMaster.deploy(NAME, SYMBOL);
+  await tokenMaster.deployed();
 
-  const Lock = await hre.ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  console.log(`Deployed TokenMaster Contract at: ${tokenMaster.address}\n`);
 
-  await lock.deployed();
+  // List 6 events
+  const occasions = [
+    {
+      name: "UFC Miami",
+      cost: tokens(3),
+      tickets: 0,
+      date: "May 31",
+      time: "6:00PM EST",
+      location: "Miami-Dade Arena - Miami, FL",
+    },
+    {
+      name: "ETH Tokyo",
+      cost: tokens(1),
+      tickets: 125,
+      date: "Jun 2",
+      time: "1:00PM JST",
+      location: "Tokyo, Japan",
+    },
+    {
+      name: "ETH Privacy Hackathon",
+      cost: tokens(0.25),
+      tickets: 200,
+      date: "Jun 9",
+      time: "10:00AM TRT",
+      location: "Turkey, Istanbul",
+    },
+    {
+      name: "Dallas Mavericks vs. San Antonio Spurs",
+      cost: tokens(5),
+      tickets: 0,
+      date: "Jun 11",
+      time: "2:30PM CST",
+      location: "American Airlines Center - Dallas, TX",
+    },
+    {
+      name: "ETH Global Toronto",
+      cost: tokens(1.5),
+      tickets: 125,
+      date: "Jun 23",
+      time: "11:00AM EST",
+      location: "Toronto, Canada",
+    },
+  ];
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  for (var i = 0; i < 5; i++) {
+    const transaction = await tokenMaster
+      .connect(deployer)
+      .list(
+        occasions[i].name,
+        occasions[i].cost,
+        occasions[i].tickets,
+        occasions[i].date,
+        occasions[i].time,
+        occasions[i].location
+      );
+
+    await transaction.wait();
+
+    console.log(`Listed Event ${i + 1}: ${occasions[i].name}`);
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
